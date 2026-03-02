62097c54d8410a1545d412868ca165d2d72549ab
QuasarDev <quasardev9@gmail.com>
2026-03-02 17:53:54 +0000

    fix: use appropriate name for import declarations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Todo } from "./modules/todo";
 import { Project } from "./modules/project";
-import { storeTodo, getStoredItem, removeItem, clearStorage } from "./modules/storage";
+import { storeProjects, getStoredProjects} from "./modules/storage";
 
 const todoList = new Todo("Shopping", "Buy groceries from the mall nearby", "Tommorow", "high");
 const todoList2 = new Todo("Shopping2", "Buy groceries from the mall nearby", "Tommorow", "high");
@@ -16,5 +16,5 @@ console.log(project);
 
 console.log(project.retrieveTodo("Shopping2"));
 
-storeTodo(project);
-console.log(getStoredItem())
+storeProjects(project);
+console.log(getStoredProjects())
